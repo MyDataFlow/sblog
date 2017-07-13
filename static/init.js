f1125da828a696944bc557d12d7e7c07358e03fd
David.Gao <david.alpha.fox@gmail.com>
2017-07-13 23:39:39 +0800

    init.js

--- static/init.js
+++ static/init.js
@@ -5,7 +5,17 @@ $(document).ready(function() {
     testEditor = editormd("editor", {
         width   : "100%",
         height  : 640,
-        syncScrolling : "single",
+        codeFold : true,
+        //syncScrolling : false,
+        saveHTMLToTextarea : true,    // 保存 HTML 到 Textarea
+        searchReplace : true,
+        watch : false,                // 关闭实时预览
+        htmlDecode : "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启
+        //toolbar  : false,             //关闭工具栏
+        //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
+        toolbarIcons : function() {
+          return ["undo", "redo", "|", "bold", "hr", "|", "preview", "watch", "|", "fullscreen", "info", "testIcon", "testIcon2", "file", "faicon"]
+        },
         path    : "/bower_components/editor.md/lib/"
     });
 
